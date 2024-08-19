1cca92f8a8899d98a082c7c91a214231b800ac16
MarcellinhoHM <marcellomontella@yahoo.com.br>
2024-08-19 09:00:02 -0300

    mais coisas feitas

---
+++ script.js
@@ -0,0 +1,19 @@
+
+let botaoSubmeter = document.getElementById("botao-submeter");
+
+botaoSubmeter.addEventListener("click", submeter);
+
+function submeter() {
+    let elementoNome = document.getElementById("nome").value;
+    let elementoIdade = document.getElementById("idade").value;
+    let elementoEmail = document.getElementById("email").value;
+    //console.log(elementoNome);
+    //console.log(elementoIdade);
+    //console.log(elementoEmail);
+
+    console.log(validaEmail(elementoEmail));
+}
+
+function validaEmail(email) {
+    return /^[\w]+@[\w]+.[a-zA-Z]+$/.test(email)
+}
